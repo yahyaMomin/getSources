6950bf8387a1a64b0ca35a42c0fd3435ed003171
yahya momin <yahyamomin06@gmail.com>
2024-12-29 16:49:58 +0530

    add cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 import express from "express";
 import routes from "./routes/routes.js";
+import cors from "cors";
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.get("/", (req, res) => {
   res.send("hello");
 });
